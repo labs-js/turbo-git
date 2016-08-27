f55bda76b840c8a77cfcd8ec18840d79f1e29a5a
Alan Campora <alan.campora@gmail.com>
2016-08-27 18:02:46 -0300

    [ADD] Selection list using inquirer

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 #!/usr/bin/env node
 var program = require('commander');
 const exec = require('child_process').exec;
+const inquirer = require('inquirer');
 
 program
     .arguments('<commitMessage>')
     .action(function (commitMessage) {
-        console.log('commitMessage: %s', commitMessage);
 
-        exec('git commit -m "' + commitMessage + '"', (error, stdout, stderr) => {
-            if (error) {
-                console.error('exec error: ${error}');
-                return;
-            }
-            console.log('stdout: ${stdout}');
-            console.log('stderr: ${stderr}');
+        //SHOW TAG LIST FOR SELECTION
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'selection',
+                message: 'Select tag for your turbo-commit',
+                choices: [
+                    '[ADD] : features commits, adding lines of code',
+                    '[DEL] : removing lines of code, code cleanup, remove old lib,unused assets, etc.',
+                    '[MOD] : modifying the way of do something, tiny changes',
+                    '[FIX] : bugfixing commits',
+                    '[REF] : commits part of a refactor',
+                    '[BRK] : breaking change commits',
+                ]
+            }]).then(function (answers) {
+                var tag = answers.selection.substring(0, 5);
+                console.log(tag);
+                commitMessage = tag + " "+ commitMessage;
+
+                //Exec git commit
+                exec('git commit -m "' + commitMessage + '"' , (error, stdout, stderr) => {
+                    if (error) {
+                        console.error('exec error: ${error}', error);
+                        return;
+                    }
+                });
+
         });
-    }).parse(process.argv);
 
-//tc hola
-//git commit -m "hola"
+    }).parse(process.argv);
