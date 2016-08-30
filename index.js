245c3986d0a6733150bf316fde6c8210b0ed838e
Alan Campora <alan.campora@gmail.com>
2016-08-29 22:45:57 -0300

    [FIX] error if nothing was added to stage area

--- index.js
+++ index.js
@@ -11,55 +11,58 @@ program
     .action(function (commitMessage) {
 
         //SHOW TAG LIST FOR SELECTION
-        inquirer.prompt([
-            {
-                type: 'list',
-                name: 'selection',
-                message: 'Select tag for your turbo-commit',
-                choices: [
+        inquirer
+            .prompt([
+                {
+                    type: 'list',
+                    name: 'selection',
+                    message: 'Select tag for your turbo-commit',
+                    choices: [
 
-                    {
-                        name: '[ADD] : features commits, adding lines of code'.green,
-                        value: "[ADD]"
-                    },
-                    {
-                        name: '[FIX] : bugfixing commits'.yellow,
-                        value: "[FIX]"
-                    },
-                    {
-                        name: '[MOD] : modifying the way of do something, tiny changes'.blue,
-                        value: "[MOD]"
-                    },
-                    {
-                        name: '[DEL] : removing lines of code, code cleanup, remove old lib,unused assets, etc.'.red,
-                        value: "[DEL]"
-                    },
-                    {
-                        name: '[REF] : commits part of a refactor'.cyan,
-                        value: "[REF]"
-                    },
-                    {
-                        name: '[BRK] : breaking change commits'.magenta,
-                        value: "[BRK]"
-                    },
-                    {
-                        name: '[MRG] : merge commits'.white,
-                        value: "[MRG]"
-                    },
-                ]
-            }]).then(function (answers) {
-            var tag = answers.selection;
-            commitMessage = tag + " " + commitMessage;
+                        {
+                            name: '[ADD] : features commits, adding lines of code'.green,
+                            value: "[ADD]"
+                        },
+                        {
+                            name: '[FIX] : bugfixing commits'.yellow,
+                            value: "[FIX]"
+                        },
+                        {
+                            name: '[MOD] : modifying the way of do something, tiny changes'.blue,
+                            value: "[MOD]"
+                        },
+                        {
+                            name: '[DEL] : removing lines of code, code cleanup, remove old lib,unused assets, etc.'.red,
+                            value: "[DEL]"
+                        },
+                        {
+                            name: '[REF] : commits part of a refactor'.cyan,
+                            value: "[REF]"
+                        },
+                        {
+                            name: '[BRK] : breaking change commits'.magenta,
+                            value: "[BRK]"
+                        },
+                        {
+                            name: '[MRG] : merge commits'.white,
+                            value: "[MRG]"
+                        },
+                    ]
+                }])
+            .then(function (answers) {
+                var tag = answers.selection;
+                var originalMessage = commitMessage;
+                commitMessage = tag + " " + commitMessage;
 
-            //Exec git commit
-            exec('git commit -m "' + commitMessage + '"', (error, stdout, stderr) => {
-                if (error) {
-                    console.error('exec error: ${error}', error);
-                    return;
-                }
+                //Exec git commit
+                exec('git commit -m "' + commitMessage + '"', (error, stdout, stderr) => {
+                    if (error) {
+                        console.log('[Error] Have you added your files before tc %s ?'.red,originalMessage);
+                        return;
+                    }
+                });
+
             });
-
-        });
 
     }).parse(process.argv);
 
