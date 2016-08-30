133250885a426edad3325b5d08b26602822cade0
Samuel Fabrizio <samuelfabrizio@gmail.com>
2016-08-29 22:57:58 -0300

    [MOD] index.js: code improvements

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 #!/usr/bin/env node
-var program = require('commander');
-const exec = require('child_process').exec;
-const inquirer = require('inquirer');
-const colors = require('colors');
+var program = require('commander'),
+    childProcess = require('child_process'),
+    inquirer = require('inquirer');
+require('colors');
 
-
-var _self = this;
 program
     .arguments('<commitMessage>')
     .action(function (commitMessage) {
-
+        "use strict";
         //SHOW TAG LIST FOR SELECTION
         inquirer.prompt([
             {
@@ -17,49 +15,43 @@ program
                 name: 'selection',
                 message: 'Select tag for your turbo-commit',
                 choices: [
-
                     {
                         name: '[ADD] : features commits, adding lines of code'.green,
-                        value: "[ADD]"
+                        value: '[ADD]'
                     },
                     {
                         name: '[FIX] : bugfixing commits'.yellow,
-                        value: "[FIX]"
+                        value: '[FIX]'
                     },
                     {
                         name: '[MOD] : modifying the way of do something, tiny changes'.blue,
-                        value: "[MOD]"
+                        value: '[MOD]'
                     },
                     {
                         name: '[DEL] : removing lines of code, code cleanup, remove old lib,unused assets, etc.'.red,
-                        value: "[DEL]"
+                        value: '[DEL]'
                     },
                     {
                         name: '[REF] : commits part of a refactor'.cyan,
-                        value: "[REF]"
+                        value: '[REF]'
                     },
                     {
                         name: '[BRK] : breaking change commits'.magenta,
-                        value: "[BRK]"
+                        value: '[BRK]'
                     },
                     {
                         name: '[MRG] : merge commits'.white,
-                        value: "[MRG]"
-                    },
+                        value: '[MRG]'
+                    }
                 ]
             }]).then(function (answers) {
-            var tag = answers.selection;
-            commitMessage = tag + " " + commitMessage;
+                var tag = answers.selection;
+                commitMessage = tag + " " + commitMessage;
+                //Exec git commit
+                childProcess.exec('git commit -m "' + commitMessage + '"', errCallback);
 
-            //Exec git commit
-            exec('git commit -m "' + commitMessage + '"', (error, stdout, stderr) => {
-                if (error) {
-                    console.error('exec error: ${error}', error);
-                    return;
+                function errCallback(error) {
+                    if (error) { console.error('exec error: ', error); }
                 }
             });
-
-        });
-
     }).parse(process.argv);
-
