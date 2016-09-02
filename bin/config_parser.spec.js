68e6810a1b469c7b34319dd5a0f73c68a0896b9b
Samuel Fabrizio <samuelfabrizio@gmail.com>
2016-09-01 23:41:52 -0300

    [ADD] config_parser: unit tests coverage

--- bin/config_parser.spec.js
+++ bin/config_parser.spec.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 var configParser = require('./config_parser');
 
-
-describe('config_parse', function () {
+describe('config_parse.js', function () {
     'use strict';
     describe('should have defined:', function () {
         it('getTagsFormat', function () {
@@ -10,6 +9,19 @@ describe('config_parse', function () {
         });
         it('getCommitConfBy', function () {
             expect(configParser.getCommitConfBy);
+        });
+    });
+    describe('getTagsFormat:', function () {
+        it('should return an array with elements', function () {
+            expect(configParser.getTagsFormat().length).toBeGreaterThan(0);
+        });
+    });
+    describe('getCommitConfBy:', function () {
+        it('should return an empty array without arguments', function () {
+            expect(configParser.getCommitConfBy().length).toBe(0);
+        });
+        it('should return results with a correct parameter type', function () {
+            expect(configParser.getCommitConfBy('tag').length).toBeGreaterThan(0);
         });
     });
 });
