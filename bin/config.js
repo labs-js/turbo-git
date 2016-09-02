0abf451a7d8f7c73a98e210c2e47f6a176114112
Samuel Fabrizio <samuelfabrizio@gmail.com>
2016-09-01 22:21:39 -0300

    [FIX] path in config json

--- bin/config.js
+++ bin/config.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 var config = {};
 
-config.tags = require('./tags.json');
+config.commits = require('./config.json').commits;
 
 config.getTagsFormat = function () {
     'use strict';
     var res = [],
         colors = require('colors/safe');
 
-    config.tags.forEach(function (val, i) {
+    config.commits.forEach(function (val, i) {
         res.push({
             'name': colors[val.color](val.tag + ' : ' + val.desc),
             'value': val.tag
