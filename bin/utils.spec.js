b97c566ba97013bd250536595eba9da8d168bfe5
Samuel Fabrizio <samuelfabrizio@gmail.com>
2016-10-03 20:59:16 +0200

    [ADD] unit test coverage to utils.js
         - Add some testing with real git repo

--- bin/utils.spec.js
+++ bin/utils.spec.js
@@ -2,15 +2,35 @@
 var consoleMock = require('console-mock'),
     console = consoleMock.create(),
     utils = require('./utils')(console),
-    childProcess = require('child_process');
+    childProcess = require('child_process'),
+    shell = require('shelljs');
 
 require('colors');
 
 
 consoleMock.enabled(false);//disable console.log prints
 
+
 describe('utils.js', function () {
     'use strict';
+
+    function gitInitInTempFolder () {
+        shell.rm('-rf', 'tmp');
+        shell.config.silent = true;
+        shell.mkdir('tmp');
+        shell.cd('tmp');
+        shell.exec('git init');
+    }
+
+    function commit (msg) {
+        shell.exec('git commit --allow-empty -m"' + msg + '"');
+    }
+
+    function finishTemp () {
+        shell.cd('../');
+        shell.rm('-rf', 'tmp');
+    }
+
     describe('should have defined', function () {
         it('checkGitRepoExistence', function () {
             expect(utils.checkGitRepoExistence).toBeDefined();
@@ -20,14 +40,14 @@ describe('utils.js', function () {
         });
     });
     describe('behavior', function () {
-        beforeEach( function () {
-            // consoleMock.enabled(false);
-            // consoleMock.historyClear();
-            spyOn(console, 'log');
-            spyOn(childProcess, 'exec');
-        });
-
         describe('showError', function () {
+            beforeEach( function () {
+                // consoleMock.enabled(true);
+                // consoleMock.historyClear();
+                spyOn(console, 'log');
+                spyOn(childProcess, 'exec');
+            });
+
             it('should return a undefined', function () {
                 expect(utils.showError()).toBeUndefined();
             });
@@ -45,6 +65,9 @@ describe('utils.js', function () {
             });
         });
         describe('checkGitRepoExistence', function () {
+            beforeEach( function() {
+                spyOn(childProcess, 'exec').andCallThrough();
+            });
             it('should return a promise obj', function () {
                 expect(typeof utils.checkGitRepoExistence()).toBe('object');
             });
@@ -54,7 +77,7 @@ describe('utils.js', function () {
             it('should have a catch method', function () {
                 expect(utils.checkGitRepoExistence().catch).toBeDefined();
             });
-            it('should execute a command', function () {
+            it('should run chilProcess exec command', function () {
                 utils.checkGitRepoExistence();
                 expect(childProcess.exec).toHaveBeenCalled();
             });
@@ -62,6 +85,25 @@ describe('utils.js', function () {
             it('should execute a git branch command', function () {
                 utils.checkGitRepoExistence();
                 expect(childProcess.exec).toHaveBeenCalledWith('git branch');
+            });
+
+            describe('with a real git repo', function () {
+                it('should resolve the promise with a git repo init', function (done) {
+                    gitInitInTempFolder();
+                    utils.checkGitRepoExistence().then(function () {
+                        expect('').toBe('');
+                        finishTemp();
+                        done();
+                    });
+                });
+                it('should reject the promise without a git repo and return a error string', function (done) {
+                    finishTemp();
+                    shell.cd('../'); //there is no repo here I guess
+                    utils.checkGitRepoExistence().catch(function (data) {
+                        expect(typeof data).toBe('string');
+                        done();
+                    });
+                });
             });
         });
     });
