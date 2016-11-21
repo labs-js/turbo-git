2cc7bda24a00e21948b5653e72a441f94d7d4803
Samuel Fabrizio <samuelfabrizio@gmail.com>
2016-11-21 10:30:16 -0300

    [MOD] cli: improve command help message

--- bin/cli.js
+++ bin/cli.js
@@ -5,7 +5,7 @@ var program = require('commander'),
 
 program
     .version(version)
-    .command('commit', 'execute a turbo commit')
-    .command('log', 'show turbo log')
-    .command('add', 'add files to commit')
+    .command('commit', 'from git: `git tc`, execute a turbo commit using a convention.')
+    .command('log', 'from git: `git tl`, show turbo log, colors with real meaning.')
+    .command('add', 'from git: `git ta`, utility for execute a git add is a easier way.')
     .parse(process.argv);
