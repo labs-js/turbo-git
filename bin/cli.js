368267ae51eb2702fd62d1cee32742fbc92124b6
Samuel Fabrizio <samuelfabrizio@gmail.com>
2018-03-03 17:25:07 +0100

    [ADD] turbo-diff: options parameters
         - now acepts --cached and all the posible options from git diff

--- bin/cli.js
+++ bin/cli.js
@@ -7,7 +7,7 @@ program
     .version(version)
     .command('add', 'from git: `git ta`, utility for execute a git add is a easier way.')
     .command('commit', 'from git: `git tc`, execute a turbo commit using a convention.')
-    .command('diff', 'from git: `git td`, execute a turbo diff (diff-to-fancy).')
+    .command('diff <options>', 'from git: `git td`, execute a turbo diff (diff-to-fancy).')
     .command('log', 'from git: `git tl`, show turbo log, colors with real meaning.')
     .command('init', 'from git: `git ti`, set a commit convention for a git repository.')
     .parse(process.argv);
