3b75cb678267c49e995d0b588691c272f9e2737e
Samuel Fabrizio <samuelfabrizio@gmail.com>
2018-03-03 17:22:18 +0100

    [ADD] turbo-diff implementation
         - add new command turbo diff

--- bin/cli.js
+++ bin/cli.js
@@ -5,8 +5,9 @@ var program = require('commander'),
 
 program
     .version(version)
-    .command('commit', 'from git: `git tc`, execute a turbo commit using a convention.')
-    .command('log', 'from git: `git tl`, show turbo log, colors with real meaning.')
     .command('add', 'from git: `git ta`, utility for execute a git add is a easier way.')
+    .command('commit', 'from git: `git tc`, execute a turbo commit using a convention.')
+    .command('diff', 'from git: `git td`, execute a turbo diff (diff-to-fancy).')
+    .command('log', 'from git: `git tl`, show turbo log, colors with real meaning.')
     .command('init', 'from git: `git ti`, set a commit convention for a git repository.')
     .parse(process.argv);
