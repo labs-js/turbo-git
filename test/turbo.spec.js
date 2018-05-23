9428e59d1fcd1c06e5d788580c006d943bfca6b3
Samuel Fabrizio <samuelfabrizio@gmail.com>
2018-05-23 15:59:49 +0200

    [MOD] turbo cmd: add basic unit test

---
+++ test/turbo.spec.js
@@ -0,0 +1,21 @@
+const turbo = require('../bin/turbo');
+
+jest.mock('commander', () => {
+    const mock = jest.genMockFromModule('commander');
+
+    mock.command = () => {
+        return mock;
+    };
+    mock.version = () => {
+        return mock;
+    };
+
+    return mock;
+});
+
+describe('turbo', () => {
+    'use strict';
+    it('should return an object', () => {
+        expect(typeof turbo).toBe('object');
+    });
+});
