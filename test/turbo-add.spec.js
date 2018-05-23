a6c0c6562238a607fdf2de7e99beba1ae00416a4
Samuel Fabrizio <samuelfabrizio@gmail.com>
2018-05-23 17:44:23 +0200

    [ADD] unit test: turbo add

---
+++ test/turbo-add.spec.js
@@ -0,0 +1,13 @@
+const turboAdd = require('../bin/turbo-add');
+
+jest.mock('turbo-git-add', () => {
+    function pepe (){}
+    pepe.prototype.init = jest.fn();
+    return pepe;
+});
+
+describe('turbo-add', () => {
+    it('should return an object', () => {
+        expect(typeof turboAdd).toBe('object');
+    });
+});
